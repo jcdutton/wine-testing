51c044a9a253b09e88200e55bddd339753d173da
James Courtier-Dutton <James@superbug.co.uk>
2021-07-27 21:45:42 +0100

    Added bft handling.

--- udp/compile.sh
+++ udp/compile.sh
@@ -2,3 +2,4 @@ i686-w64-mingw32-gcc -c -o udpserver.o win-udpserver.c
 i686-w64-mingw32-gcc -o udpserver.exe udpserver.o -lws2_32
 gcc -o udpclient udpclient.c
 gcc -o udpserver udpserver.c
+gcc -o udpbftserver udpbftserver.c
---
+++ udp/udpbftserver.c
@@ -0,0 +1,137 @@
+// Server side implementation of UDP. This is the receiver.
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <poll.h>
+#include <linux/bpf.h>
+#include <linux/filter.h>
+#include <errno.h>
+#include <error.h>
+
+#define PORT	 8080
+#define MAXLINE 1024
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
+#endif
+
+//static void attach_cbpf(int fd, uint16_t mod)
+static void attach_cbpf(int fd)
+{
+	struct sock_filter code[] = {
+		/* A = 0 */ // Like Windows sockets
+		{ BPF_LD  | BPF_W | BPF_IMM, 0, 0, 0 },
+		/* A = 1 */ // Like Linux sockets
+		//{ BPF_LD  | BPF_W | BPF_IMM, 0, 0, 1 },
+		/* return A */
+		{ BPF_RET | BPF_A, 0, 0, 0 },
+		};
+	struct sock_fprog p = {
+		.len = ARRAY_SIZE(code),
+		.filter = code,
+	};
+
+	if (setsockopt(fd, SOL_SOCKET, SO_ATTACH_REUSEPORT_CBPF, &p, sizeof(p)))
+		error(1, errno, "failed to set SO_ATTACH_REUSEPORT_CBPF");
+}
+
+int main() {
+	int sockfd1,sockfd2;
+	int i;
+	char buffer[MAXLINE];
+	struct sockaddr_in servaddr, cliaddr;
+	struct pollfd poll_set[2];
+	int poll_ret = 0;
+	
+	// Creating socket file descriptor
+	if ( (sockfd1 = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket1 creation failed");
+		exit(EXIT_FAILURE);
+	}
+	// Creating socket file descriptor
+	if ( (sockfd2 = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket2 creation failed");
+		exit(EXIT_FAILURE);
+	}
+	int enable = 1;
+	if (setsockopt(sockfd1, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) < 0) {
+		perror("setsockopt(SO_REUSEADDR) failed on sockfd1");
+	}
+	enable = 1;
+	if (setsockopt(sockfd2, SOL_SOCKET, SO_REUSEPORT, &enable, sizeof(int)) < 0) {
+		perror("setsockopt(SO_REUSEADDR) failed on sockfd2");
+	}
+	attach_cbpf(sockfd1);
+	attach_cbpf(sockfd1);
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd1, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed for sockfd1");
+		exit(EXIT_FAILURE);
+	}
+	// Bind the socket with the server address
+	if ( bind(sockfd2, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed for sockfd2");
+		exit(EXIT_FAILURE);
+	}
+	
+	
+	int len, n;
+
+	len = sizeof(cliaddr); //len is value/resuslt
+
+	for (i = 0; i < 2; ++i) {
+		poll_set[i].fd = -1;
+		poll_set[i].events = POLLIN;
+		poll_set[i].revents = 0;
+	}
+	poll_set[0].fd = sockfd1;
+	poll_set[1].fd = sockfd2;
+
+	int timeout_ms = 1000;
+	for (;;) {
+		poll_ret = poll(poll_set, 2, timeout_ms);
+		if (poll_ret > 0) {
+			for (int n = 0; n < 2; n++) {
+				printf("%d:fd=%d\n", n, poll_set[n].fd);
+				printf("%d:events=%d\n", n, poll_set[n].events);
+				printf("%d:revents=%d\n", n, poll_set[n].revents);
+				if (poll_set[n].revents != 0) {
+					int tmp = recvfrom(poll_set[n].fd, (char *)buffer, MAXLINE,
+								MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+								&len);
+					buffer[tmp] = '\0'; // FIXME: buffer overflow risk
+					uint32_t host1 = ntohl(cliaddr.sin_addr.s_addr);
+					printf("From Server: recvfrom socket offset %d, IP address %d.%d.%d.%d, Port %d,  Client : %s\n",
+							n,
+							host1 >> 24 & 0xff,
+							host1 >> 16 & 0xff,
+							host1 >> 8 & 0xff,
+							host1 & 0xff,
+							cliaddr.sin_port,
+							buffer);
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+
